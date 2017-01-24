8bf2d266881ee6a03fefb86e34be58af865cedd6
Swarada K <kswara20@gmail.com>
2017-01-24 14:19:02 +0530

    Deploy shell script

---
+++ DeployToPCFDev.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+###############################################################################
+# Name			: 	DeployToPCFDev.sh
+# Author		: 	Swarada Kalekar
+# Created on	: 	Jan 2017
+# Description 	: 	Script is written to deploy the application 
+# 					on PCF Dev Environment. Script will accept 
+#					User Details, Organization, Space, Application details
+###############################################################################
+
+if [ ${#} -ne 5 ]; then
+	echo "Usage: DeployToPCFDev.sh <HostURL> <UserId> <Password> <Org> <Space> <ApplicationName>"
+else
+	export hostURL=$1
+	export userId=$2
+	export password=$3
+	export org=$4
+	export space=$5
+	export appName=$6
+	
+	cf login -a ${hostURL} --skip-ssl-validation -u ${userId} -p ${password} -o ${org} -s ${space}
+	cf push --hostname ${appName} > logs/deployLogs.log 2>&1
+	cf logs ${appName} --recent > logs/appRecentLogs.log 2>&1
+	
+	
+fi
